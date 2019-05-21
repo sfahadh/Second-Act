d4d6715db8bdfed2684b0a69d385069303a805a5
Fahad Hussain <sfahadh14@gmail.com>
2019-05-20 22:13:33 -0400

    complete question 3 file

--- src/components/Questions/Question3.js
+++ src/components/Questions/Question3.js
@@ -29,23 +29,23 @@ export default class Question1 extends Component {
 
     render() {
         const { nextQuestion, revealSubmit } = this.state
-        if(nextQuestion) return <Redirect to="/question-3"/>
+        // if(nextQuestion) return <Redirect to="/quiz/question-3"/>
 
         return (
         <div className="questions-page">
             <form className="question-form">
-                <h3 className="question-header">How much are you willing to spend on school?</h3>
+                <h3 className="question-header">How much time can you commit to a program in a week?</h3>
                 <h4>(you can select multiple choices)</h4>
                 <div className="answer-choice first" onClick={this.handleRevealSubmit}>
-                    <h3>$10,000+</h3>
+                    <h3>1 - 6 hours</h3>
                 </div>
 
                 <div className="answer-choice second" onClick={this.handleRevealSubmit}>
-                    <h3>$1000 - $10,000</h3>
+                    <h3>7 - 14 hours</h3>
                 </div>
 
                 <div className="answer-choice" onClick={this.handleRevealSubmit}>
-                    <h3>$0 - $999</h3>
+                    <h3>15 - 40 hours</h3>
                 </div>
                 {
                     !!revealSubmit ? <button onClick={this.handleSubmit}>Submit</button> : null
