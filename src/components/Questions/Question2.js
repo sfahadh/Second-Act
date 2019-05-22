f39c6416a1ec877791e1f3b6175106a412e09c67
Fahad Hussain <sfahadh14@gmail.com>
2019-05-22 09:07:06 -0400

    allow users to select multiple answers in question 1

--- src/components/Questions/Question2.js
+++ src/components/Questions/Question2.js
@@ -9,14 +9,47 @@ export default class Question1 extends Component {
         this.state = {
             nextQuestion: false,
             revealSubmit: false,
+            choice1: true,
+            choice2: true,
+            choice3: true,
         }
         this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleRevealSubmit = this.handleRevealSubmit.bind(this);
+        this.toggleChoice1 = this.toggleChoice1.bind(this);
+        this.toggleChoice2 = this.toggleChoice2.bind(this);
+        this.toggleChoice3 = this.toggleChoice3.bind(this);
     }
 
-    handleRevealSubmit() {
-        this.setState({
-            revealSubmit: true,
+
+    toggleChoice1() {
+        this.setState((prevState) => {
+            return {
+                revealSubmit: true,
+                choice1: !prevState.choice1,
+                choice2: false,
+                choice3: false
+            }
+        })
+    }
+
+    toggleChoice2() {
+        this.setState((prevState) => {
+            return {
+                revealSubmit: true,
+                choice2: !prevState.choice2,
+                choice1: false,
+                choice3: false
+                }
+        })
+    }
+
+    toggleChoice3() {
+        this.setState((prevState) => {
+            return {
+                revealSubmit: true,
+                choice3: !prevState.choice3,
+                choice2: false,
+                choice1: false
+            }
         })
     }
 
@@ -28,23 +61,26 @@ export default class Question1 extends Component {
     }
 
     render() {
-        const { nextQuestion, revealSubmit } = this.state
+        const { nextQuestion, revealSubmit, choice1, choice2, choice3, } = this.state
         if(nextQuestion) return <Redirect to="/question-3"/>
 
         return (
         <div className="questions-page">
             <form className="question-form">
                 <h3 className="question-header">How much are you willing to spend on school?</h3>
-                <h4>(you can select multiple choices)</h4>
-                <div className="answer-choice first" onClick={this.handleRevealSubmit}>
+                <h4></h4>
+                <div className={ choice1 ? 'answer-choice first' : 'condition'} 
+                    onClick={this.toggleChoice1}>
                     <h5>$10,000+</h5>
                 </div>
 
-                <div className="answer-choice second" onClick={this.handleRevealSubmit}>
+                <div className={ choice2 ? 'answer-choice second' : 'condition'} 
+                    onClick={this.toggleChoice2}>
                     <h5>$1000 - $10,000</h5>
                 </div>
 
-                <div className="answer-choice" onClick={this.handleRevealSubmit}>
+                <div className={ choice3 ? 'answer-choice' : 'condition'} 
+                    onClick={this.toggleChoice3}>
                     <h5>$0 - $999</h5>
                 </div>
                 {
